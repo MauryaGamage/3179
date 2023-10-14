c082a5ddbca009e2805238e568c0d5d8ef5cbd95
mgam0010 <mgam0010@student.monash.edu>
2023-10-14 17:06:37 +1100

    new changes

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,15 +1,14 @@
 var vg_1 = "global_cost.vg.json";
-vegaEmbed("#world_map", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+vegaEmbed("#world_map", vg_1, { actions: false }).then(function(result) {
+  // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_2 = "bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_2).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+vegaEmbed("#bar_chart", vg_2, { actions: false }).then(function(result) {
+  // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_3 = "housing_city.vg.json";
-vegaEmbed("#housing_chart", vg_3).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+vegaEmbed("#housing_chart", vg_3, { actions: false }).then(function(result) {
+  // Access the Vega view instance as result.view
 }).catch(console.error);
-
